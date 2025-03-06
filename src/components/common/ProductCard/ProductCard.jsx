56a58891c8b241cd9f586912ce51e4dba27f1aff
GHGabrielHurt <127798822+GHGabrielHurt@users.noreply.github.com>
2025-03-06 16:50:52 -0300

    Update ProductCard.jsx

--- src/components/common/ProductCard/ProductCard.jsx
+++ src/components/common/ProductCard/ProductCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button } from "@mui/material";
-import './productCard.css';
+import './ProductCard.css';
 import { Link } from "react-router-dom";
 
 export const ProductCard = (props) => {
